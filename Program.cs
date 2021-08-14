9913e8b80d81d9ca1805320eea04ab8799ebc442
kaiorod <caiorodrigo2775@gmail.com>
2021-08-13 23:33:48 -0300

    finaliza OlaMundo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Olá usuário, eu me chamo ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Caio!");
+            Console.ResetColor();
+        }
+    }
+}
